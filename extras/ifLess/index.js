0169249c4ca10c6abdcb8ac8e7ee17e49e64713c
luiz-ortega <luizngust@hotmail.com>
2020-12-08 21:25:51 -0300

    feat: add "positive return" strategy

--- extras/ifLess/index.js
+++ extras/ifLess/index.js
@@ -11,3 +11,29 @@ function isMaleOverAge(age, gender) {
 }
 
 console.log(isMaleOverAge(18, "F"));
+
+// Always return positive
+
+let name = "Johny";
+
+// if (!(name.trim() === "")) {
+//   console.log("valid name 1");
+// }
+
+if (name.trim() !== "") {
+  console.log("valid name 1");
+}
+
+if (name.trim() !== "" && name.trim().length >= 3) {
+  console.log("valid name 2");
+}
+
+// function isValidName(name) {
+//   return name.trim() !== "" && name.trim().length >= 3;
+// }
+
+function isValidName(name) {
+  return name.trim() !== "" && name.trim().length >= 3;
+}
+
+if (isValidName(name)) console.log(true);
