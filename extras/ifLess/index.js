467eaa0b990474be32fcc13aaf1268a69186d174
luiz-ortega <luizngust@hotmail.com>
2020-12-08 21:02:26 -0300

    extras/ifLess

---
+++ extras/ifLess/index.js
@@ -0,0 +1,13 @@
+// purposeless condition
+
+function isMaleOverAge(age, gender) {
+  // if (age >= 18 && gender === "M") {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
+
+  return age >= 18 && gender === "M";
+}
+
+console.log(isMaleOverAge(18, "F"));
